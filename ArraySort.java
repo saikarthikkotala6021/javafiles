25d8bf114c55f4db1af49c4cdd1fb6c06acfe368
KOTALA SAI KARTHIK <93485351+saikarthikkotala6021@users.noreply.github.com>
2023-05-06 11:50:51 +0530

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class ArraySort {
     public static void main(String[] args) {
-        System.out.println("Hello")
+        System.out.println("Hello");
         
     }
     
