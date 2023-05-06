9a0ce98e25aaf442e094b2867b7ab5e38fad8445
KOTALA SAI KARTHIK <93485351+saikarthikkotala6021@users.noreply.github.com>
2023-05-06 11:48:47 +0530

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -1,22 +1,7 @@
 import java.util.*;
 public class ArraySort {
-    static boolean Sort(int a[], int n){
-        int min = a[0];
-        for(int i=0;i<a.length;i++){
-            if(min>a[i])
-                return false;
-        }
-        return true;
-
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-        System.out.println(Sort(a, n));
+        System.out.println("Hello")
         
     }
     
